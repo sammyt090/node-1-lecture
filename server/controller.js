823add170c33453923305417328b1c2c91d4d5c9
sammyt090 <samueltanner231@gmail.com>
2020-05-19 10:51:36 -0700

    Udated

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,7 @@
 const users = require("../data.json")
 
+let id = users[users.length -1].id +1
+
 module.exports = {
     getAllUsers: (req, res) => {
         const {email} = req.query
@@ -22,6 +24,55 @@ module.exports = {
         else{
             res.status(404).send("User not found")
         }
+    },
+
+
+    createUser: (req, res) =>{
+        const {first_name, last_name, email}= req.body
+
+        const newUser = {id, first_name, last_name, email}
+        users.push(newUser)
+        id++
+
+        res.status(200).send(users)
+    },
+
+    editUser: (req, res) => {
+        const {user_id} = req.params
+        const {first_name, last_name, email} = req.body
+
+        const index = users.findIndex(element => element.id === +user_id)
+        if (index === -1){
+            return res.status(404).send("User not found")
+        }
+
+        // const updatedUser = {id: +user_id, first_name, last_name, email}
+
+        const updatedUser = {
+            id: +user_id,
+            first_name: first_name || users[index].first_name,
+            last_name: last_name || users[index].last_name,
+            email: email || users[index].email
+        }
+
+        users[index] = updatedUser
+
+        res.status(200).send(users)
+    },
+
+    deleteUser: (req, res) => {
+        const {user_id} = req.params
+
+        const index = users.findIndex(element => element.id === +user_id)
+        if(index === -1){
+            return res.status(404).send("User not found")
+        }
+
+        users.splice(index, 1)
+
+        res.status(200).send(users)
+
+
     }
 
 
--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const SERVER_PORT = 4000
 
 app.use(express.json())
 
+app.use(function(req, res, next){
+    console.log("We got a request cap'n")
+    next()
+})
+
 app.get('/api/andrew', (req, res) => {
     console.log("I got a request from /api/andrew")
     res.status(200).send('Andrew')
@@ -13,6 +18,9 @@ app.get('/api/andrew', (req, res) => {
 
 app.get('/api/users', ctrl.getAllUsers)
 app.get('/api/users/:user_id', ctrl.getUserById)
+app.post('/api/users', ctrl.createUser)
+app.put('/api/users/:user_id', ctrl.editUser)
+app.delete('/api/users/:user_id', ctrl.deleteUser)
 
 app.listen(SERVER_PORT, () => 
 console.log(`Server running on port ${SERVER_PORT}`))
